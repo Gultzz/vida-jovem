2109ce2a10b5f9de341e72401058c60823420e94
Gultzz <gustavo.schulr@gmail.com>
2023-03-06 21:46:57 -0300

    Build e Scroll

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -2,10 +2,16 @@ import React from "react";
 import * as S from "./styles";
 import VidaJovemLogo from "../../assets/vida_jovem_logo.png";
 import VidaJovemMiniLogo from "../../assets/vida_jovem_mini_logo.png";
-import { useNavigate } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 export function Header() {
   const navigate = useNavigate();
+
+  function documentToScoll(e: any, id: string) {
+    e.preventDefault();
+    let doc = document.getElementById(id);
+    doc && doc.scrollIntoView({ behavior: "smooth" });
+  }
 
   return (
     <S.HeaderFull>
@@ -18,10 +24,18 @@ export function Header() {
           />
         </div>
         <S.LinksContainer>
-          <S.Link onClick={() => navigate("#sejasocio")}>Seja Sócio</S.Link>
-          <S.Link onClick={() => navigate("#galeria")}>Galeria</S.Link>
-          <S.Link onClick={() => navigate("#quemsomos")}>Quem Somos</S.Link>
-          <S.Link onClick={() => navigate("#contato")}>Contato</S.Link>
+          <S.Link onClick={(e) => documentToScoll(e, "sejasocio")}>
+            Seja Sócio
+          </S.Link>
+          <S.Link onClick={(e) => documentToScoll(e, "galeria")}>
+            Galeria
+          </S.Link>
+          <S.Link onClick={(e) => documentToScoll(e, "quemsomos")}>
+            Quem Somos
+          </S.Link>
+          <S.Link onClick={(e) => documentToScoll(e, "contato")}>
+            Contato
+          </S.Link>
           <S.LinkButton onClick={() => navigate("/score")}>
             Pontuação
           </S.LinkButton>
